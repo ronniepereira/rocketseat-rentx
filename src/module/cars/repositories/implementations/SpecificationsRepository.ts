e8821ff4814a344c7d28f9f531732eeb498e1613
backendenext <back@enext.com.br>
2022-01-11 21:49:17 -0300

    Commit inicial

---
+++ src/module/cars/repositories/implementations/SpecificationsRepository.ts
@@ -0,0 +1,40 @@
+import { Specification } from "../../model/Specification";
+import { ISpecificationsRepository, ICreateSpecificationDTO } from "../ISpecificationsRepository";
+
+class SpecificationRepository implements ISpecificationsRepository {
+    private specifications: Specification[]
+
+    private static INSTANCE: SpecificationRepository;
+
+    private constructor() {
+        this.specifications = []
+    }
+
+    public static getInstance(): SpecificationRepository {
+        if (!SpecificationRepository.INSTANCE) {
+            SpecificationRepository.INSTANCE = new SpecificationRepository();
+        }
+
+        return SpecificationRepository.INSTANCE
+    }
+
+    create({ description, name }: ICreateSpecificationDTO): void {
+        const specification = new Specification();
+
+        Object.assign(specification, {
+            name,
+            description,
+            created_at: new Date()
+        })
+
+        this.specifications.push(specification)
+    }
+
+    findByName(name: string): Specification {
+        const specification = this.specifications.find(specification => specification.name === name)
+        return specification
+    }
+
+}
+
+export { SpecificationRepository }
